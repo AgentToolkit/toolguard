b7b10748bfa6db0c2f491a4f8d2d837857f60c35
Ella Rabinovich <ellarabinovich@dhcp-9-147-174-48.givatayim.il.ibm.com>
2026-01-22 14:46:55 +0200

    a comment on reuse model

--- src/toolguard/buildtime/utils/datamodel_codegen.py
+++ src/toolguard/buildtime/utils/datamodel_codegen.py
@@ -13,7 +13,7 @@ def run(oas_file: Path):
             "pydantic_v2.BaseModel",  # "typing.TypedDict",
             "--collapse-root-models",
             # "--force-optional",
-            "--reuse-model",
+            "--reuse-model",  # https://github.com/koxudaxi/datamodel-code-generator/blob/4661406431a17b17c2ad0335589bcb12123fd45d/docs/model-reuse.md
             "--enum-field-as-literal",
             "all",
             "--input-file-type",
