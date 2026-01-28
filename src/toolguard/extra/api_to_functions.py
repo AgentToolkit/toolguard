9dcb540af6379761d731a177847d172f852ca1fc
DAVID BOAZ <DAVIDBO@il.ibm.com>
2026-01-28 07:29:10 +0200

    only public methods

--- src/toolguard/extra/api_to_functions.py
+++ src/toolguard/extra/api_to_functions.py
@@ -9,9 +9,10 @@ def api_cls_to_functions(api_cls: type) -> List[Callable]:
         api_cls: The class type to extract functions from.
 
     Returns:
-        A list of callable functions found in the class.
+        A list of public callable functions found in the class (excludes private/protected methods starting with underscore).
     """
     return [
         member
         for name, member in inspect.getmembers(api_cls, predicate=inspect.isfunction)
+        if not name.startswith("_")
     ]
